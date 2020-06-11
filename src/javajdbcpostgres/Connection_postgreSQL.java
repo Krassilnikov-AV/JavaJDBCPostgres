8d36bd981a6ecb5d0ff4b728ad8e5e78d2b75b38
Krassilnikov-AV <61631173+Krassilnikov-AV@users.noreply.github.com>
2020-06-11 12:06:56 +0300

    Update Connection_postgreSQL.java

--- src/javajdbcpostgres/Connection_postgreSQL.java
+++ src/javajdbcpostgres/Connection_postgreSQL.java
@@ -5,6 +5,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// Пересмотреть и добавить строчные поля для соединения 
+// в которых указать имя БД, логин, пароль
+
 
 public class Connection_postgreSQL {
 
